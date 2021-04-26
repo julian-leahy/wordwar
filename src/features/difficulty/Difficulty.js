f099cf627fa1b919b3bd82591135e0b7039c9fb7
Julian Leahy <julian.leahy.dev@gmail.com>
2021-04-26 13:29:29 +1200

    add fade effect to display rules

--- src/features/difficulty/Difficulty.js
+++ src/features/difficulty/Difficulty.js
@@ -4,11 +4,13 @@ import { AIWords, difficulty, findWords, isNewGame } from '../board/boardSlice';
 import CustomButton from '../buttons/CustomButton';
 import Rules from '../rules/Rules';
 import './Difficulty.scss';
+import { CSSTransition } from 'react-transition-group';
 
 function Difficulty() {
 
     const dispatch = useDispatch();
-    const [showRules, setShowRules] = useState(false)
+    const [showRules, setShowRules] = useState(false);
+    const nodeRef = React.createRef(null);
 
     const setDifficulty = (level) => {
         dispatch(findWords());
@@ -18,21 +20,38 @@ function Difficulty() {
     }
 
     const toggleRules = () => {
-        setShowRules(!showRules)
+        setShowRules(!showRules);
     }
 
     return (
         <div className='difficulty'>
-            <div className='difficulty-inner'>
-                <div className='title'>Word War</div>
-                <div className='btn-group'>
-                    <CustomButton level='easy' set={setDifficulty} />
-                    <CustomButton level='hard' set={setDifficulty} />
-                    <CustomButton level='expert' set={setDifficulty} />
+            <div className='difficulty-wrap'>
+                <div className='difficulty-inner'>
+                    <div className='title'>Word War</div>
+                    <div className='btn-group'>
+                        <CustomButton level='easy' set={setDifficulty} />
+                        <CustomButton level='hard' set={setDifficulty} />
+                        <CustomButton level='expert' set={setDifficulty} />
+                    </div>
+
+                    <button aria-label='open rules' onClick={toggleRules} className='btn-help show-rules'>Rules</button>
                 </div>
-                <button aria-label='open rules' onClick={toggleRules} className='btn-help show-rules'>Rules</button>
-                {showRules && <Rules close={toggleRules} />}
+                <CSSTransition
+                    nodeRef={nodeRef}
+                    in={showRules}
+                    timeout={300}
+                    unmountOnExit
+                    classNames={'loaded'}
+                >
+
+                    <div ref={nodeRef}>
+                        <Rules close={toggleRules} />
+                    </div>
+
+                </CSSTransition>
             </div>
+
+
 
         </div>
     )

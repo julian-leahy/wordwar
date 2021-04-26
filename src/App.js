c22246e8b629e8840f1ea3889b7ea9340f0150f2
Julian Leahy <julian.leahy.dev@gmail.com>
2021-04-26 13:37:28 +1200

    add transition to show board

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import './App.scss';
 import Board from './features/board/Board';
 import { selectGame } from './features/board/boardSlice';
 import Difficulty from './features/difficulty/Difficulty';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
-  const gameState = useSelector(selectGame)
+  const gameState = useSelector(selectGame);
+  const nodeRef = React.createRef(null);
   return (
     <div className="App">
       {
-        !gameState ? <Board roundTime={180} /> : <Difficulty />
+        gameState && <Difficulty />
       }
+      <CSSTransition
+        nodeRef={nodeRef}
+        in={!gameState}
+        timeout={300}
+        unmountOnExit
+        classNames={'show'}
+      >
+
+        <div ref={nodeRef}>
+          <Board roundTime={180} />
+        </div>
+
+      </CSSTransition>
     </div>
   )
 }
